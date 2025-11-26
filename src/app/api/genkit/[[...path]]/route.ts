4ec108602558fba2afec93e6065160a897a6cd32
astromanreal <astroman@gmail.com>
2025-11-26 23:43:08 +0530

    second commit

--- src/app/api/genkit/[[...path]]/route.ts
+++ src/app/api/genkit/[[...path]]/route.ts
@@ -1,4 +1,4 @@
-import {getGenkitAPIRoute} from '@genkit-ai/next';
+import getGenkitAPIRoute from '@genkit-ai/next';
 import '@/ai/dev'; // Ensure flows are registered
 
 export const {GET, POST} = getGenkitAPIRoute();
